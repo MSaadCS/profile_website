02ba30eb6116b20a169df504ea40e5d5b07a4c9b
saad <mohamed.saad@gbrands.com>
2023-11-10 20:06:21 +0200

    fix path issue

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./assets/imgs/logo.png">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
     <link rel="stylesheet" href="./assets/fonts/fonts.css">
     <link rel="stylesheet" href="./assets/css/fontawesome.min.css"/>
     <link rel="stylesheet" href="./assets/css/fontawesome.css"/>
